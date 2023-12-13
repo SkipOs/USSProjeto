606e64c2796e22cce63797590b1a546c0c369f03
Skip0s <eduhenrimart1@gmail.com>
2023-12-13 12:22:30 -0300

    Update CadPaci.cs

--- USSProjeto/CadPaci.cs
+++ USSProjeto/CadPaci.cs
@@ -29,14 +29,30 @@ namespace USSProjeto
         {
             string connetionString = "server=localhost;database=uss_banco;uid=root;pwd=admin;";
             using (MySqlConnection cnn = new MySqlConnection(connetionString))
+            try
             {
                 cnn.Open();
-                //inserção
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = cnn;
 
+                cmd.CommandText = "INSERT INTO tb_paciente(nome, nascimento, sexo, cpf, tel, email, endereco, complemento) VALUES('" + nome.Text + "', " + nasc.Text + ", " + sex.Text + ", " + cpf.Text + ", " + tel.Text + ", '" + email.Text + "', '" + end.Text + "', '" + comp.Text + "'); ";
+                cmd.ExecuteNonQuery();
                 MessageBox.Show("Paciente inserido com sucesso.");
                 cnn.Close();
             }
-        }
+            catch
+            {
+                MessageBox.Show("Erro na inserção.");
+            }
+            finally
+            {
+                this.Hide();
+                MenuBase menu = new MenuBase();
+                menu.Closed += (s, args) => this.Close();
+                menu.Show();
+                this.Dispose();
+            }
 
+        }
     }
 }
